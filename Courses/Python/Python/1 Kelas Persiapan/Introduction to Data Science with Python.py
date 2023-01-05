b595c9b09081a42b3cb938e29ecda6c8400eb324
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-05 23:46:44 +0700

    Learn

--- Courses/Python/Python/1 Kelas Persiapan/Introduction to Data Science with Python.py
+++ Courses/Python/Python/1 Kelas Persiapan/Introduction to Data Science with Python.py
@@ -1,2 +1,55 @@
+# # [Python “Hello World” & Statement]
+
+# In[1]:
+
+
 print("Hello World.")
 print("Saya Aksara, baru belajar Python.")
+
+
+# # [Variables di Python]
+
+# In[3]:
+
+
+bil1 = 10
+Bil_2 = 20
+Frasa = "Halo Dunia"
+bil1, Bil_2 = 10, 20
+salam = "Selamat Pagi"; Penutup = "Salam Sejahtera"
+
+
+# # [Comments di Python]
+
+# In[4]:
+
+
+#perintah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
+
+
+# # [Tugas Praktek]
+
+# In[5]:
+
+
+text = "Belajar Python di DQLab."
+print(list(text))
+print(tuple(text))
+print(set(text))
+
+
+# # [Menggunakan Library di Python]
+
+# In[6]:
+
+
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
