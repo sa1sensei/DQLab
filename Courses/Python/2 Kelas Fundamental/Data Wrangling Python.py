3571b3708e0cda7bb45c33190e4e25c22ce04cfb
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-14 18:46:45 +0700

    Learn

--- Courses/Python/2 Kelas Fundamental/Data Wrangling Python.py
+++ Courses/Python/2 Kelas Fundamental/Data Wrangling Python.py
@@ -1,185 +1,75 @@
-# # # # # # # # # 
+# # # # # # # # # Menampilkan informasi statistik dengan Numpy
 # In[1]:
 
+import pandas as pd
+
+csv_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/shopping_data.csv")
+
+print(csv_data.describe(exclude=["O"]))
 
 
 
-
-# # # # # # # # # 
+# # # # # # # # # Melakukan pengecekan untuk nilai NULL yang ada
 # In[2]:
 
+import pandas as pd
+
+csv_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/shopping_data_missingvalue.csv")
+
+print(csv_data.isnull().values.any())
 
 
 
-
-# # # # # # # # # 
+# # # # # # # # # Mengisi dengan Mean
 # In[3]:
 
 
+import pandas as pd
+
+csv_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/shopping_data_missingvalue.csv")
+print(csv_data.mean())
+print("Dataset yang masih terdapat nilai kosong ! :")
+print(csv_data.head(10))
+
+csv_data=csv_data.fillna(csv_data.mean())
+print("Dataset yang sudah diproses Handling Missing Values dengan Mean :")
+print(csv_data.head(10))
 
 
-
-# # # # # # # # # 
+# # # # # # # # # Mengisi dengan Median
 # In[4]:
 
+import pandas as pd
+
+csv_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/shopping_data_missingvalue.csv")
+print("Dataset yang masih terdapat nilai kosong ! :")
+print(csv_data.head(10))
+
+csv_data=csv_data.fillna(csv_data.median())
+print("Dataset yang sudah diproses Handling Missing Values dengan Median :")
+print(csv_data.head(10))
 
 
-# # # # # # # # # 
+# # # # # # # # # Praktek Normalisasi menggunakan Scikit Learn pada Python
 # In[5]:
 
 
-# # # # # # # # # 
-# In[6]:
-
-
-
-# # # # # # # # # 
-# In[7]:
-
-
-
-# # # # # # # # # 
-# In[8]:
-
-
-
-# # # # # # # # # 
-# In[9]:
-
-
-
-# # # # # # # # # 
-# In[10]:
-
-
-
-# # # # # # # # # 
-# In[11]:
-
-
-
-# # # # # # # # # 
-# In[12]:
-
-
-
-# # # # # # # # # 
-# In[13]:
-
-
-
-# # # # # # # # # 
-# In[14]:
-
-
-
-# # # # # # # # # 
-# In[15]:
-
-
-
-# # # # # # # # # 
-# In[16]:
-
-
-
-# # # # # # # # # 
-# In[17]:
-
-
-
-# # # # # # # # # 
-# In[18]:
-
-
-
-# # # # # # # # # 
-# In[19]:
-
-
-
-# # # # # # # # # 
-# In[20]:
-
-
-
-# # # # # # # # # 
-# In[21]:
-
-
-
-# # # # # # # # # 
-# In[22]:
-
-
-
-# # # # # # # # # 
-# In[23]:
-
-
-
-# # # # # # # # # 
-# In[24]:
-
-
-
-# # # # # # # # # 
-# In[25]:
-
-
-
-# # # # # # # # # 
-# In[26]:
-
-
-
-# # # # # # # # # 
-# In[27]:
-
-
-
-# # # # # # # # # 
-# In[28]:
-
-
-
-# # # # # # # # # 
-# In[29]:
-
-
-
-# # # # # # # # # 
-# In[30]:
-
-
-
-# # # # # # # # # 
-# In[31]:
-
-
-
-# # # # # # # # # 
-# In[32]:
-
-
-
-# # # # # # # # # 
-# In[33]:
-
-
-
-# # # # # # # # # 
-# In[34]:
-
-
-
-# # # # # # # # # 
-# In[35]:
-
-
-
-# # # # # # # # # 
-# In[36]:
-
-
-
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+csv_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/shopping_data.csv")
+array = csv_data.values
+X = array[:,2:5] #memisahkan fitur dari dataset. 
+Y = array[:,0:1]  #memisahkan class dari dataset
+
+dataset=pd.DataFrame({"Customer ID":array[:,0],"Gender":array[:,1],"Age":array[:,2],"Income":array[:,3],"Spending Score":array[:,4]})
+print("dataset sebelum dinormalisasi :")
+print(dataset.head(10))
+
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0,1)) #inisialisasi normalisasi MinMax
+data = min_max_scaler.fit_transform(X) #transformasi MinMax untuk fitur
+dataset = pd.DataFrame({"Age":data[:,0],"Income":data[:,1],"Spending Score":data[:,2],"Customer ID":array[:,0],"Gender":array[:,1]})
+
+print("dataset setelah dinormalisasi :")
+print(dataset.head(10))
