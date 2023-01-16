3740f64e616aa81b9fb6540149b4a0207c94f906
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 19:10:46 +0700

    Learn

--- Courses/SQL/2 Kelas Fundamental/Memanfaatkan Operator Logika di SQL.sql
+++ Courses/SQL/2 Kelas Fundamental/Memanfaatkan Operator Logika di SQL.sql
@@ -1,128 +1,100 @@
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
+-- Praktikum
+SELECT DISTINCT
+	Customer_ID, Product,
+    Average_transaction,
+    Average_transaction >= 1000000 is_eligible
+FROM summary_transaction;
+
+-- Logika AND
+SELECT
+   Customer_ID, Product, average_transaction_amount,
+   product = 'Jaket' AND average_transaction_amount >= 1000000 loyal_buyer_jaket
+FROM data_retail
+WHERE product = 'Jaket'
+
+
+-- Logika OR
+SELECT 
+     Customer_ID, 
+     Product, 
+     average_transaction_amount,
+     product = 'Jaket' OR product = 'Baju' buyer_fashion
+FROM data_retail;
+
+
+-- Logika NOT
+SELECT 
+       *
+FROM data_retail
+WHERE NOT product = 'Jaket';
+
+-- Logika XOR
+SELECT 
+    Customer_ID,
+    Product,
+    Average_Transaction_Amount,
+    product = 'Jaket' XOR average_transaction_amount > 1000000 logika_xor
+FROM data_retail;
+
+
+-- Pemanfaatan Operator Logika dalam Perintah Where
+SELECT DISTINCT
+     *
+FROM data_retail
+WHERE Product = "Jaket" AND Average_Transaction_Amount >= 1000000;
+
+-- Pemanfaatan Operator Logika dalam Konstruksi Case When
+SELECT DISTINCT
+Customer_ID,
+Product,
+Average_Transaction,
+Count_Transaction,
+CASE
+     WHEN Average_transaction > 2000000 and Count_Transaction > 30 then 'Platinum Member'
+     WHEN Average_transaction between 1000000 and 2000000 and Count_Transaction between 20 and 30 then 'Gold Member'
+     WHEN Average_transaction < 1000000 and Count_Transaction<20 then 'Silver Member'
+         ELSE "Other Membership" end as Membership
+FROM summary_transaction
+
+
+-- Praktikum
+SELECT DISTINCT
+Customer_ID,
+Product,
+Average_transaction,
+Count_Transaction,
+CASE
+	WHEN Average_transaction <1000000 then '4-5x Email dalam seminggu'
+    WHEN Average_transaction >1000000 then '1-2x Email dalam seminggu'
+END AS frekuensi_email
+FROM summary_transaction;
+
+
+-- Perintah Where dan Logika And
+SELECT DISTINCT
+Customer_ID
+FROM summary_transaction
+WHERE Average_transaction < 1000000 and product =  'Sepatu';
+
+
+-- Menyiapkan Report Penjualan
+SELECT DISTINCT
+Product Produk,
+avg(Average_transaction) 'Jumlah transaksi (Rupiah)',
+sum(Count_Transaction) 'Barang terjual'
+FROM summary_transaction
+GROUP BY Product
+
+-- Tugas Pertamaku
+SELECT DISTINCT
+	Customer_ID
+FROM data_retail_undian
+WHERE Unik_produk > 3 AND Rata_rata_transaksi > 1500000
+
+
+-- Tugas Keduaku
+SELECT DISTINCT
+	Customer_ID
+FROM data_retail_undian
+WHERE NOT (Unik_produk >= 3 AND Rata_rata_transaksi >= 1500000)
 
--- Courses/SQL/2 Kelas Fundamental/Menggunakan Subquery pada SQL.sql
+++ Courses/SQL/2 Kelas Fundamental/Menggunakan Subquery pada SQL.sql
@@ -1,128 +1,78 @@
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
-
--- 
-
-
--- 
-
--- 
-
-
--- 
-
+-- Subquery dalam klausa WHERE
+#Menghitung customer yang membeli Sepatu dan Jaket
+SELECT COUNT(DISTINCT customer_id) total_buyer_Sepatu_Jaket
+FROM data_retail
+WHERE product = 'Sepatu' 
+AND customer_id IN (
+ 	SELECT distinct Customer_ID
+    FROM data_retail
+    WHERE product = 'Jaket');
+
+#Menghitung customer yang membeli Sepatu dan Baju
+SELECT COUNT(DISTINCT customer_id) total_buyer_Sepatu_Baju
+FROM data_retail
+WHERE product = 'Sepatu' 
+AND customer_id IN (
+ 	SELECT distinct Customer_ID
+    FROM data_retail
+    WHERE product = 'Baju');
+    
+#Menghitung customer yang membeli Sepatu dan Tas
+SELECT COUNT(DISTINCT customer_id) total_buyer_Sepatu_Tas
+FROM data_retail
+WHERE product = 'Sepatu'  
+AND customer_id IN (
+ 	SELECT distinct Customer_ID
+    FROM data_retail
+    WHERE product = 'Tas');
+
+
+-- Subquery dalam klausa JOIN
+SELECT a.Customer_ID, a.Transaksi_Sepatu, b.Transaksi_Jaket
+FROM (select customer_id, sum(count_transaction) transaksi_sepatu
+      from data_retail
+      where product = 'Sepatu'
+      group by 1) A
+JOIN (select Customer_ID, sum(Count_Transaction) transaksi_jaket
+      from data_retail
+      where product = 'Jaket'
+      group by 1) B on a.Customer_ID = b.Customer_ID
+ORDER BY 2 DESC,3 DESC
+
+
+-- Mencari Rata-Rata Total Transaksi untuk Produk Sepatu
+
+SELECT AVG(Count_Transaction) avg_transaksi
+FROM data_retail
+WHERE product = 'Sepatu'
+
+-- Subquery untuk menghasilkan nilai kolom pada konstruksi SELECT
+SELECT
+	Customer_ID, Count_Transaction,
+    (
+    SELECT AVG(Count_Transaction)
+    		FROM data_retail
+            WHERE product = 'Sepatu'
+    ) Avg_Transaction
+FROM data_retail
+WHERE product = 'Sepatu'
+
+-- Mini Project - Analisis penjualan DQ-Shop
+SELECT 
+A.Product, 
+A.total_buyer, 
+D.loyal_customer
+FROM (
+   SELECT Product, COUNT(DISTINCT Customer_ID) total_buyer
+   FROM data_retail
+   GROUP BY 1) A
+JOIN (
+   SELECT B.Product, COUNT(DISTINCT Customer_ID) loyal_customer
+   FROM data_retail B
+   JOIN (
+    SELECT Product, AVG(Count_Transaction) AS Count_Transaction
+    FROM data_retail 
+    GROUP BY 1
+   ) C ON C.Product = B.Product AND B.Count_Transaction > C.Count_Transaction
+   GROUP BY 1
+   ) D ON A.Product = D.Product;
