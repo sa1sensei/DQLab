ec07e2e7ae8fbea34aefc91d9e87021145d2793b
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 17:49:32 +0700

    Learn

--- Courses/Python/3 Kelas Penerapan di Industri/Data Science Project Analisis Data COVID19 di Dunia & ASEAN.py
+++ Courses/Python/3 Kelas Penerapan di Industri/Data Science Project Analisis Data COVID19 di Dunia & ASEAN.py
@@ -1,185 +1,357 @@
-# # # # # # # # # 
+# # # # # # # # # Library yang dibutuhkan
+
 # In[1]:
 
+import numpy as np
+import pandas as pd
 
 
 
+# # # # # # # # # Membaca Data COVID-19 Dunia
 
-# # # # # # # # # 
 # In[2]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+covid_url = 'https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json'
+df_covid_worldwide = pd.read_json(covid_url)
+
+print("Ukuran dataset: %d kolom dan %d baris.\n" % df_covid_worldwide.shape)
+print("Lima data teratas:\n", df_covid_worldwide.head())
+print("\nLima data terbawah:\n", df_covid_worldwide.tail())
 
 
 
+# # # # # # # # # Reformat Data Frame COVID-19 Dunia
 
-# # # # # # # # # 
 # In[3]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+
+print("Informasi data frame awal:")
+df_covid_worldwide.info()
+
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index()
+
+print("\nInformasi data frame setelah set index kolom date:")
+df_covid_worldwide.info()
 
 
 
+# # # # # # # # # Missing Value di DataFrame COVID-19 Dunia
 
-# # # # # # # # # 
 # In[4]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# # # # # # # # # 
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index()
+
+print("Jumlah missing value tiap kolom:")
+print(df_covid_worldwide.isna().sum())
+
+df_covid_worldwide.dropna(inplace=True)
+
+print("\nJumlah missing value tiap kolom setelah didrop:")
+print(df_covid_worldwide.isna().sum())
+
+# # # # # # # # # Membaca Data Countries
+
 # In[5]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
-# # # # # # # # # 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+print(df_countries.head())
+
+
+# # # # # # # # # Merge Covid19 Data dan Countries
+
 # In[6]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# # # # # # # # # 
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+print(df_covid_denormalized.head())
+
+# # # # # # # # # Menghitung Fatality Ratio
+
 # In[7]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+df_covid_denormalized["fatality_ratio"] = (df_covid_denormalized["deaths"]/df_covid_denormalized["confirmed_cases"])
+print(df_covid_denormalized.head())
 
 
-# # # # # # # # # 
+# # # # # # # # # Negara-negara dengan Fatality Ratio Tertinggi
+
 # In[8]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+df_covid_denormalized["fatality_ratio"] = df_covid_denormalized["deaths"]/df_covid_denormalized["confirmed_cases"]
+
+df_top_20_fatality_rate = df_covid_denormalized.sort_values("fatality_ratio", ascending=False).head(20)
+print(df_top_20_fatality_rate[["geo_id","country_name","fatality_ratio"]])
 
 
-# # # # # # # # # 
+# # # # # # # # # Kondisi Fatality ratio Tertinggi di Bulan Agustus 2020
+
 # In[9]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# # # # # # # # # 
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+df_covid_denormalized_august = df_covid_denormalized.loc["08-2020"].groupby("country_name").sum()
+
+df_covid_denormalized_august["fatality_ratio"] = df_covid_denormalized_august["deaths"]/df_covid_denormalized_august["confirmed_cases"]
+
+df_top_20_fatality_rate_on_august = df_covid_denormalized_august.sort_values(by="fatality_ratio", ascending=False).head(20)
+print(df_top_20_fatality_rate_on_august["fatality_ratio"])
+
+# # # # # # # # # Visualisasi Negara dengan Fatality Ratio Tertinggi di Bulan Agustus 2020
+
 # In[10]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# # # # # # # # # 
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+df_covid_denormalized_august = df_covid_denormalized.loc["2020-08"].groupby("country_name").sum()
+
+df_covid_denormalized_august["fatality_ratio"] = df_covid_denormalized_august["deaths"]/df_covid_denormalized_august["confirmed_cases"]
+
+df_top_20_fatality_rate_on_august = df_covid_denormalized_august.sort_values("fatality_ratio", ascending=False).head(20)
+
+# Visulisasi
+import matplotlib.pyplot as plt
+plt.figure(figsize=(8,8))
+df_top_20_fatality_rate_on_august["fatality_ratio"].sort_values().plot (kind="barh", color="coral")
+plt.title("Top 20 Highest Fatality Rate Countries", fontsize=18, color="b")
+plt.xlabel("Fatality Rate", fontsize=14)
+plt.ylabel("Country Name", fontsize=14)
+plt.grid(axis="x")
+plt.tight_layout()
+plt.show()
+
+# # # # # # # # # Data Frame Kasus COVID-19 ASEAN
+
 # In[11]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# # # # # # # # # 
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+asean_country_id = ["ID", "MY", "SG", "TH", "VN"]
+filter_list = [(df_covid_denormalized["geo_id"]==country_id).to_numpy() for country_id in asean_country_id] #kenapa ke to_numpy?
+filter_array = np.column_stack(filter_list).sum(axis=1, dtype="bool") #untuk apa menggunakan np.column_stack?
+df_covid_denormalized_asean = df_covid_denormalized[filter_array].sort_index()
+
+print("Cek nilai unik di kolom 'country_name':", df_covid_denormalized_asean["country_name"].unique())
+print(df_covid_denormalized_asean.head())
+
+# # # # # # # # # Kapankah Kasus Pertama COVID-19 di ASEAN
+
 # In[12]:
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# # # # # # # # # 
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+asean_country_id = ["ID", "MY", "SG", "TH", "VN"]
+filter_list = [(df_covid_denormalized["geo_id"]==country_id).to_numpy() for country_id in asean_country_id]
+filter_array = np.column_stack(filter_list).sum(axis=1, dtype="bool")
+df_covid_denormalized_asean = df_covid_denormalized[filter_array].sort_index()
+
+print("The first case popped up in each of 5 ASEAN countries:")
+for country_id in asean_country_id:
+    asean_country = df_covid_denormalized_asean[df_covid_denormalized_asean["geo_id"]==country_id]
+    first_case = asean_country[asean_country["confirmed_cases"]>0][["confirmed_cases","geo_id","country_name"]]
+    print(first_case.head(1))
+
+# # # # # # # # # Kasus Covid-19 di ASEAN mulai Bulan Maret 2020
+
 # In[13]:
 
 
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
 
-# # # # # # # # # 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+asean_country_id = ["ID", "MY", "SG", "TH", "VN"]
+filter_list = [(df_covid_denormalized["geo_id"]==country_id).to_numpy() for country_id in asean_country_id]
+filter_array = np.column_stack(filter_list).sum(axis=1, dtype="bool")
+df_covid_denormalized_asean = df_covid_denormalized[filter_array].sort_index()
+
+df_covid_denormalized_asean_march_onward = df_covid_denormalized_asean[df_covid_denormalized_asean.index>="2020-03-01"]
+print(df_covid_denormalized_asean_march_onward.head())
+
+# # # # # # # # # Visualisasi Kasus COVID-19 di ASEAN
+
 # In[14]:
 
-
-
-# # # # # # # # # 
-# In[15]:
-
-
-
-# # # # # # # # # 
-# In[16]:
-
-
-
-# # # # # # # # # 
-# In[17]:
-
-
-
-# # # # # # # # # 
-# In[18]:
-
-
-
-# # # # # # # # # 
-# In[19]:
-
-
-
-# # # # # # # # # 
-# In[20]:
-
-
-
-# # # # # # # # # 
-# In[21]:
-
-
-
-# # # # # # # # # 
-# In[22]:
-
-
-
-# # # # # # # # # 
-# In[23]:
-
-
-
-# # # # # # # # # 
-# In[24]:
-
-
-
-# # # # # # # # # 
-# In[25]:
-
-
-
-# # # # # # # # # 
-# In[26]:
-
-
-
-# # # # # # # # # 
-# In[27]:
-
-
-
-# # # # # # # # # 
-# In[28]:
-
-
-
-# # # # # # # # # 
-# In[29]:
-
-
-
-# # # # # # # # # 
-# In[30]:
-
-
-
-# # # # # # # # # 
-# In[31]:
-
-
-
-# # # # # # # # # 
-# In[32]:
-
-
-
-# # # # # # # # # 
-# In[33]:
-
-
-
-# # # # # # # # # 
-# In[34]:
-
-
-
-# # # # # # # # # 
-# In[35]:
-
-
-
-# # # # # # # # # 
-# In[36]:
-
-
+import numpy as np
+import pandas as pd
+pd.set_option("display.max_columns", None)
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+covid_url = "https://storage.googleapis.com/dqlab-dataset/covid19_worldwide_2020.json"
+df_covid_worldwide = pd.read_json(covid_url)
+df_covid_worldwide = df_covid_worldwide.set_index("date").sort_index().dropna()
+
+countries_url = "https://storage.googleapis.com/dqlab-dataset/country_details.json"
+df_countries = pd.read_json(countries_url)
+
+df_covid_denormalized = pd.merge(df_covid_worldwide.reset_index(), df_countries, on="geo_id").set_index("date")
+
+asean_country_id = ["ID", "MY", "SG", "TH", "VN"]
+filter_list = [(df_covid_denormalized["geo_id"]==country_id).to_numpy() for country_id in asean_country_id]
+filter_array = np.column_stack(filter_list).sum(axis=1, dtype="bool")
+df_covid_denormalized_asean = df_covid_denormalized[filter_array].sort_index()
+
+df_covid_denormalized_asean_march_onward = df_covid_denormalized_asean[df_covid_denormalized_asean.index>="2020-03-01"]
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.figure(figsize=(16,6))
+sns.lineplot(data=df_covid_denormalized_asean_march_onward, 
+             x=index.df_covid_denormalized_asean_march_onward, 
+             y="confirmed_cases", 
+             hue="country_name",
+             linewidth=2)
+plt.xlabel('Record Date', fontsize=14)
+plt.ylabel('Total Cases', fontsize=14)
+plt.title('Comparison of COVID19 Cases in 5 ASEAN Countries', color="b", fontsize=18)
+plt.grid()
+plt.tight_layout()
+plt.show()
 
