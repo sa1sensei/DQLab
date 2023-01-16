1da2aa4dbfb90c3c59c011f9e6f795d748c254f2
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 17:35:11 +0700

    Learn

--- Courses/Python/3 Kelas Penerapan di Industri/Data Science in Telco Data Cleansing.py
+++ Courses/Python/3 Kelas Penerapan di Industri/Data Science in Telco Data Cleansing.py
@@ -1,185 +1,251 @@
-# # # # # # # # # 
+# # # # # # # # # Import Library dan Dataset
+
 # In[1]:
 
 
+#import library
+import pandas as pd
+pd.options.display.max_columns = 50
+
+#import dataset
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
+
+#Tampilkan jumlah baris dan kolom
+print(df_load.shape)
+
+#Tampilkan 5 data teratas
+print(df_load.head(5))
+
+#Jumlah ID yang unik
+print(df_load.customerID.nunique())
 
 
 
-# # # # # # # # # 
+# # # # # # # # # Memfilter ID Number Pelanggan Format Tertentu
+
 # In[2]:
 
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
+
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+print('Hasil jumlah ID Customer yang terfilter adalah', df_load['customerID'].count())
 
 
 
+# # # # # # # # # Memfilter Duplikasi ID Number Pelanggan
 
-# # # # # # # # # 
 # In[3]:
 
 
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+
+# Drop Duplicate Rows
+df_load.drop_duplicates()
+# Drop duplicate ID sorted by Periode
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates(['customerID'])
+print('Hasil jumlah ID Customer yang sudah dihilangkan duplikasinya (distinct) adalah',df_load['customerID'].count())
 
 
+# # # # # # # # # Mengatasi Missing Values dengan Penghapusan Rows
 
-# # # # # # # # # 
 # In[4]:
 
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
+
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+df_load.drop_duplicates()
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates('customerID')
+
+print('Total missing values data dari kolom Churn',df_load['Churn'].isnull().sum())
+# Dropping all Rows with spesific column (churn)
+df_load.dropna(subset=['Churn'],inplace=True)
+print('Total Rows dan kolom Data setelah dihapus data Missing Values adalah',df_load.shape)
 
 
-# # # # # # # # # 
+# # # # # # # # # Mengatasi Missing Values dengan Pengisian Nilai tertentu
+
 # In[5]:
 
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
 
-# # # # # # # # # 
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+df_load.drop_duplicates()
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates('customerID')
+df_load.dropna(subset=['Churn'],inplace=True)
+
+print('Status Missing Values :',df_load.isnull().values.any())
+print('\nJumlah Missing Values masing-masing kolom, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+
+#handling missing values Tenure fill with 11
+df_load['tenure'].fillna(11, inplace=True)
+
+#Loop
+#Handling missing values num vars (except Tenure)
+for col_name in list(['MonthlyCharges','TotalCharges']):
+	median = df_load[col_name].median()
+	df_load[col_name].fillna(median, inplace=True)
+
+print('\nJumlah Missing Values setelah di imputer datanya, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+
+
+# # # # # # # # # Mendeteksi adanya Outlier (Boxplot)
+
 # In[6]:
 
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
+
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+df_load.drop_duplicates()
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates('customerID')
+df_load.dropna(subset=['Churn'],inplace=True)
+
+df_load['tenure'].fillna(11, inplace=True)
+for col_name in list(['MonthlyCharges','TotalCharges']):
+    median = df_load[col_name].median()
+    df_load[col_name].fillna(median, inplace=True)
+
+print('\nPersebaran data sebelum ditangani Outlier: ')
+print(df_load[['tenure','MonthlyCharges','TotalCharges']].describe())
+
+# Creating Box Plot
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Misal untuk kolom tenure
+plt.figure() # untuk membuat figure baru
+sns.boxplot(x=df_load['tenure'])
+plt.show()
+# dan seterusnya untuk kedua kolom yang tersisa secara berurut
+plt.figure() # untuk membuat figure baru
+sns.boxplot(x=df_load['MonthlyCharges'])
+plt.show()
+plt.figure() # untuk membuat figure baru
+sns.boxplot(x=df_load['TotalCharges'])
+plt.show()
 
 
-# # # # # # # # # 
+# # # # # # # # # Mengatasi Outlier
+
 # In[7]:
 
 
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
 
-# # # # # # # # # 
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+df_load.drop_duplicates()
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates('customerID')
+df_load.dropna(subset=['Churn'],inplace=True)
+
+df_load['tenure'].fillna(11, inplace=True)
+for col_name in list(['MonthlyCharges','TotalCharges']):
+    median = df_load[col_name].median()
+    df_load[col_name].fillna(median, inplace=True)
+
+# Your code goes here
+# Handling with IQR
+Q1 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.25)
+Q3 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.75)
+
+IQR = Q3 - Q1
+maximum = Q3 + (1.5*IQR)
+print('Nilai Maximum dari masing-masing Variable adalah: ')
+print(maximum)
+minimum = Q1 - (1.5*IQR)
+print('\nNilai Minimum dari masing-masing Variable adalah: ')
+print(minimum)
+
+more_than = (df_load > maximum)
+lower_than = (df_load < minimum)
+df_load = df_load.mask(more_than, maximum, axis=1)
+df_load = df_load.mask(lower_than, minimum, axis=1)
+
+print('\nPersebaran data setelah ditangani Outlier: ')
+print(df_load[['tenure','MonthlyCharges','TotalCharges']].describe())
+
+
+# # # # # # # # # Mendeteksi Nilai yang tidak Standar
+
 # In[8]:
 
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
 
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+df_load.drop_duplicates()
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates('customerID')
+df_load.dropna(subset=['Churn'],inplace=True)
 
-# # # # # # # # # 
+df_load['tenure'].fillna(11, inplace=True)
+for col_name in list(['MonthlyCharges','TotalCharges']):
+    median = df_load[col_name].median()
+    df_load[col_name].fillna(median, inplace=True)
+
+Q1 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.25)
+Q3 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.75)
+IQR = Q3 - Q1
+maximum  = Q3 + (1.5*IQR)
+minimum = Q1 - (1.5*IQR)
+
+more_than     = (df_load > maximum)
+lower_than    = (df_load < minimum)
+df_load       = df_load.mask(more_than, maximum, axis=1) 
+df_load       = df_load.mask(lower_than, minimum, axis=1)
+
+#Loop
+# Masukkan variable
+for col_name in list(['gender','SeniorCitizen','Partner','Dependents','PhoneService','MultipleLines','InternetService','OnlineSecurity','OnlineBackup','DeviceProtection','TechSupport','StreamingTV','StreamingMovies','Contract','PaperlessBilling','PaymentMethod','Churn']):
+	print('\nUnique Values Count \033[1m' + 'Before Standardized \033[0m Variable',col_name)
+	print(df_load[col_name].value_counts())
+
+# # # # # # # # # Menstandarisasi Variable Kategorik
+
 # In[9]:
 
-
-
-# # # # # # # # # 
-# In[10]:
-
-
-
-# # # # # # # # # 
-# In[11]:
-
-
-
-# # # # # # # # # 
-# In[12]:
-
-
-
-# # # # # # # # # 
-# In[13]:
-
-
-
-# # # # # # # # # 
-# In[14]:
-
-
-
-# # # # # # # # # 
-# In[15]:
-
-
-
-# # # # # # # # # 
-# In[16]:
-
-
-
-# # # # # # # # # 
-# In[17]:
-
-
-
-# # # # # # # # # 
-# In[18]:
-
-
-
-# # # # # # # # # 
-# In[19]:
-
-
-
-# # # # # # # # # 
-# In[20]:
-
-
-
-# # # # # # # # # 
-# In[21]:
-
-
-
-# # # # # # # # # 
-# In[22]:
-
-
-
-# # # # # # # # # 
-# In[23]:
-
-
-
-# # # # # # # # # 
-# In[24]:
-
-
-
-# # # # # # # # # 
-# In[25]:
-
-
-
-# # # # # # # # # 
-# In[26]:
-
-
-
-# # # # # # # # # 
-# In[27]:
-
-
-
-# # # # # # # # # 
-# In[28]:
-
-
-
-# # # # # # # # # 
-# In[29]:
-
-
-
-# # # # # # # # # 
-# In[30]:
-
-
-
-# # # # # # # # # 
-# In[31]:
-
-
-
-# # # # # # # # # 
-# In[32]:
-
-
-
-# # # # # # # # # 
-# In[33]:
-
-
-
-# # # # # # # # # 
-# In[34]:
-
-
-
-# # # # # # # # # 
-# In[35]:
-
-
-
-# # # # # # # # # 
-# In[36]:
-
-
-
+import pandas as pd
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
+
+df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
+df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
+df_load.drop_duplicates()
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates('customerID')
+df_load.dropna(subset=['Churn'],inplace=True)
+
+df_load['tenure'].fillna(11, inplace=True)
+for col_name in list(['MonthlyCharges','TotalCharges']):
+    median = df_load[col_name].median()
+    df_load[col_name].fillna(median, inplace=True)
+
+Q1 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.25)
+Q3 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.75)
+IQR = Q3 - Q1
+maximum  = Q3 + (1.5*IQR)
+minimum = Q1 - (1.5*IQR)
+
+more_than     = (df_load > maximum)
+lower_than    = (df_load < minimum)
+df_load       = df_load.mask(more_than, maximum, axis=1) 
+df_load       = df_load.mask(lower_than, minimum, axis=1)
+
+df_load = df_load.replace(['Wanita','Laki-Laki','Churn','Iya'],['Female','Male','Yes','Yes'])
+
+# Masukkan variable
+for col_name in list(['gender','Dependents','Churn']):
+	print('\nUnique Values Count \033[1m' + 'After Standardized \033[0mVariable',col_name)
+	print(df_load[col_name].value_counts())
+	
