ceeb1d1d245e591bca9ad3e6f0c40f219ec64c74
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 18:01:37 +0700

    Learn

--- Courses/Python/3 Kelas Penerapan di Industri/Data Analyst Project Business Decision Research.py
+++ Courses/Python/3 Kelas Penerapan di Industri/Data Analyst Project Business Decision Research.py
@@ -1,185 +1,450 @@
-# # # # # # # # # 
+# # # # # # # # # Importing Data dan Inspection
+
 # In[1]:
 
 
+import pandas as pd
+
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+
+print('Lima data teratas:')
+print(df.head())
+
+print('\nInfo dataset:')
+print(df.info())
 
 
+# # # # # # # # # Data Cleansing
 
-# # # # # # # # # 
 # In[2]:
 
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+
+# Kolom First_Transaction
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+# Kolom Last_Transaction
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+
+print('Lima data teratas:')
+print(df.head())
+
+print('\nInfo dataset:')
+print(df.info())
 
 
+# # # # # # # # # Churn Customers
 
-# # # # # # # # # 
 # In[3]:
 
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+
+# Pengecekan transaksaksi terakhir dalam dataset
+print(max(df['Last_Transaction']))
+
+# Klasifikasikan customer yang berstatus churn atau tidak
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False
+
+print('Lima data teratas:')
+print(df.head())
+
+print('\nInfo dataset:')
+print(df.info())
 
 
+# # # # # # # # # Menghapus kolom yang tidak diperlukan
 
-# # # # # # # # # 
 # In[4]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+
+# Hapus kolom-kolom yang tidak diperlukan
+del df['no']
+del df['Row_Num']
+
+# Cetak lima data teratas
+print(df.head())
 
 
-# # # # # # # # # 
+
+# # # # # # # # # Customer acquisition by year
+
 # In[5]:
 
 
-# # # # # # # # # 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+
+import matplotlib.pyplot as plt
+
+# Kolom tahun transaksi pertama
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+
+# Kolom tahun transaksi terakhir
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df_year = df.groupby(['Year_First_Transaction'])['Customer_ID'].count()
+df_year.plot(x='Year_First_Transaction', y='Customer_ID', kind='bar', title='Graph of Customer Acquisition')
+plt.xlabel('Year_First_Transaction')
+plt.ylabel('Num_of_Customer')
+plt.tight_layout()
+plt.show()
+
+
+
+
+# # # # # # # # # Transaction by year
+
 # In[6]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
 
+import matplotlib.pyplot as plt
+plt.clf()
+df_year = df.groupby(['Year_First_Transaction'])['Count_Transaction'].sum()
+df_year.plot(x='Year_First_Transaction', y='Count_Transaction', kind='bar', title='Graph of Transaction Customer')
+plt.xlabel('Year_First_Transaction')
+plt.ylabel('Num_of_Transaction')
+plt.tight_layout()
+plt.show()
 
-# # # # # # # # # 
+# # # # # # # # # Average transaction amount by year
+
 # In[7]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# # # # # # # # # 
+plt.clf()
+sns.pointplot(data = df.groupby(['Product', 'Year_First_Transaction']).mean().reset_index(),
+			  x='Year_First_Transaction',
+			  y='Average_Transaction_Amount',
+			  hue='Product')
+plt.tight_layout()
+plt.show()
+
+# # # # # # # # # Proporsi churned customer untuk setiap produk
+
 # In[8]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
 
+import matplotlib.pyplot as plt
+plt.clf()
 
-# # # # # # # # # 
+# Melakukan pivot data
+df_piv = df.pivot_table(index='is_churn',
+						columns='Product',
+						values='Customer_ID',
+						aggfunc='count',
+						fill_value=0)
+
+# Mendapatkan Proportion Churn by Product
+plot_product = df_piv.count().sort_values(ascending=False).head(5).index
+
+# Plot pie chartnya
+df_piv = df_piv.reindex(columns=plot_product)
+df_piv.plot.pie(subplots=True,
+				figsize=(10, 7),
+				layout=(-1, 2),
+				autopct='%1.0f%%',
+				title='Proportion Churn by Product')
+plt.tight_layout()
+plt.show()
+
+# # # # # # # # # Distribusi kategorisasi count transaction
+
 # In[9]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
 
+import matplotlib.pyplot as plt
+plt.clf()
+# Kategorisasi jumlah transaksi
+def func(row):
+	if row['Count_Transaction'] == 1:
+		val = '1. 1'
+	elif (row['Count_Transaction'] >1 and row['Count_Transaction'] <= 3):
+		val ='2. 2 - 3'
+	elif (row['Count_Transaction'] >3 and row['Count_Transaction'] <= 6):
+		val ='3. 4 - 6'
+	elif (row['Count_Transaction'] >6 and row['Count_Transaction'] <= 10):
+		val ='4. 7 - 10'
+	else:
+		val ='5. >10'
+	return val
+# Tambahkan kolom baru
+df['Count_Transaction_Group'] = df.apply(func, axis=1)
+df_year = df.groupby(['Count_Transaction_Group'])['Customer_ID'].count()
+df_year.plot(x='Count_Transaction_Group', y='Customer_ID', kind='bar', title='Customer Distribution by Count Transaction Group')
+plt.xlabel('Count_Transaction_Group')
+plt.ylabel('Num_of_Customer')
+plt.tight_layout()
+plt.show()
 
-# # # # # # # # # 
+# # # # # # # # # Distribusi kategorisasi average transaction amount
+
 # In[10]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
+
+import matplotlib.pyplot as plt
+
+plt.clf()
+# Kategorisasi rata-rata besar transaksi
+def f(row):
+    if (row['Average_Transaction_Amount'] > 100000 and row ['Average_Transaction_Amount'] <= 250000):
+        val ='1. 100.000 - 250.000'
+    elif (row['Average_Transaction_Amount'] > 250000 and row['Average_Transaction_Amount'] <= 500000):
+        val ='2. >250.000 - 500.000'
+    elif (row['Average_Transaction_Amount'] > 500000 and row['Average_Transaction_Amount'] <= 750000):
+        val = '3. >500.000 - 750.000'
+    elif (row['Average_Transaction_Amount'] > 750000 and row['Average_Transaction_Amount'] <= 1000000):
+        val = '4. >750.000 - 1.000.000'
+    elif (row['Average_Transaction_Amount'] > 1000000 and row['Average_Transaction_Amount'] <= 2500000):
+        val = '5. >1.000.000 - 2.500.000'
+    elif (row['Average_Transaction_Amount'] > 2500000 and row['Average_Transaction_Amount'] <= 5000000):
+        val = '6. >2.500.000 - 5.000.000'
+    elif (row ['Average_Transaction_Amount'] > 5000000 and row ['Average_Transaction_Amount'] <= 10000000):
+        val = '7. >5.000.000 - 10.000.000'
+    else:
+        val = '8. >10.000.000'
+    return val
+# Tambahkan kolom baru
+df['Average_Transaction_Amount_Group'] = df.apply(f, axis=1)
+
+df_year = df.groupby(['Average_Transaction_Amount_Group'])['Customer_ID'].count()
+df_year.plot(x='Average_Transaction_Amount_Group', y='Customer_ID', kind='bar', title='Customer Distribution by Average Transaction Amount Group')
+plt.xlabel('Average_Transaction_Amount_Group')
+plt.ylabel('Num_of_Customer')
+plt.tight_layout()
+plt.show()
 
 
-# # # # # # # # # 
+# # # # # # # # # Feature Columns dan Target
+
 # In[11]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
+
+# Feature column: Year_Diff
+df['Year_Diff']=df['Year_Last_Transaction']-df['Year_First_Transaction']
+
+# Nama-nama feature columns
+feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff']
+
+# Features variable
+X = df[feature_columns]
+
+# Target variable
+y = df['is_churn']
 
 
-# # # # # # # # # 
+
+# # # # # # # # # Split X dan y ke dalam bagian training dan testing
+
 # In[12]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
+
+df['Year_Diff']=df['Year_Last_Transaction']-df['Year_First_Transaction']
+feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff']
+
+X = df[feature_columns] 
+y = df['is_churn']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 
-# # # # # # # # # 
+# # # # # # # # # Train, predict dan evaluate
+
 # In[13]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
+
+df['Year_Diff']=df['Year_Last_Transaction']-df['Year_First_Transaction']
+feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff']
+
+X = df[feature_columns] 
+y = df['is_churn'] 
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+# Inisiasi model logreg
+logreg = LogisticRegression()
+
+# fit the model with data
+logreg.fit(X_train, y_train)
+
+# Predict model
+y_pred = logreg.predict(X_test)
+
+# Evaluasi model menggunakan confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+print('Confusion Matrix:\n', cnf_matrix)
 
 
-# # # # # # # # # 
+
+# # # # # # # # # Visualisasi Confusion Matrix
+
 # In[14]:
 
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
 
-# # # # # # # # # 
+df['Year_Diff']=df['Year_Last_Transaction']-df['Year_First_Transaction']
+feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff']
+
+X = df[feature_columns] 
+y = df['is_churn'] 
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+cnf_matrix = confusion_matrix(y_test, y_pred)
+
+# import required modules
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.clf()
+
+# name of classes
+class_names = [0, 1]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap='YlGnBu', fmt='g')
+ax.xaxis.set_label_position('top')
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.tight_layout()
+plt.show()
+
+
+
+# # # # # # # # # Accuracy, Precision, dan Recall
+
 # In[15]:
 
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
 
+df['Year_Diff']=df['Year_Last_Transaction']-df['Year_First_Transaction']
+feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff']
 
-# # # # # # # # # 
-# In[16]:
+X = df[feature_columns] 
+y = df['is_churn'] 
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
-# # # # # # # # # 
-# In[17]:
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+cnf_matrix = confusion_matrix(y_test, y_pred)
 
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
-
-# # # # # # # # # 
-# In[18]:
-
-
-
-# # # # # # # # # 
-# In[19]:
-
-
-
-# # # # # # # # # 
-# In[20]:
-
-
-
-# # # # # # # # # 
-# In[21]:
-
-
-
-# # # # # # # # # 
-# In[22]:
-
-
-
-# # # # # # # # # 
-# In[23]:
-
-
-
-# # # # # # # # # 
-# In[24]:
-
-
-
-# # # # # # # # # 
-# In[25]:
-
-
-
-# # # # # # # # # 
-# In[26]:
-
-
-
-# # # # # # # # # 
-# In[27]:
-
-
-
-# # # # # # # # # 
-# In[28]:
-
-
-
-# # # # # # # # # 
-# In[29]:
-
-
-
-# # # # # # # # # 
-# In[30]:
-
-
-
-# # # # # # # # # 
-# In[31]:
-
-
-
-# # # # # # # # # 
-# In[32]:
-
-
-
-# # # # # # # # # 
-# In[33]:
-
-
-
-# # # # # # # # # 
-# In[34]:
-
-
-
-# # # # # # # # # 
-# In[35]:
-
-
-
-# # # # # # # # # 
-# In[36]:
-
+#Menghitung Accuracy, Precision, dan Recall
+print('Accuracy :', accuracy_score(y_test, y_pred))
+print('Precision:', precision_score(y_test, y_pred, average='micro'))
+print('Recall :', recall_score(y_test, y_pred, average='micro'))
 
 
