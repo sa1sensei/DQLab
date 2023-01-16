c588dd2e193c7c427737bb3ed89142da6911cda8
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 16:10:01 +0700

    Learn

--- Courses/Python/2 Kelas Fundamental/Data Visualization with Python Matplotlib for Beginner - Part 1.py
+++ Courses/Python/2 Kelas Fundamental/Data Visualization with Python Matplotlib for Beginner - Part 1.py
@@ -1,185 +1,329 @@
-# # # # # # # # # 
+# # # # # # # # # Pengenalan Dataset
+
 # In[1]:
 
+import pandas as pd
+dataset = pd.read_csv(https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv)
+print('Ukuran dataset: %d baris dan %d kolom\n' % dataset.shape)
+print('Lima data teratas:')
+print(dataset.head())
 
 
 
 
-# # # # # # # # # 
+# # # # # # # # # Penambahan Kolom Order Month pada Dataset
+
 # In[2]:
 
+import pandas as pd
+import datetime
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+
+print(dataset.head())
 
 
 
 
-# # # # # # # # # 
+# # # # # # # # # Penambahan Kolom GMV pada Dataset
+
 # In[3]:
 
+import pandas as pd
+import datetime
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+
+dataset['gmv'] = dataset['item_price'] * dataset['quantity']
+print('Ukuran dataset: %d baris dan %d kolom\n' % dataset.shape)
+print('Lima data teratas:')
+print(dataset.head())
 
 
 
+# # # # # # # # # Membuat Data Agregat
 
-# # # # # # # # # 
 # In[4]:
 
+import pandas as pd
+import datetime
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
 
-# # # # # # # # # 
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+print(monthly_amount)
+
+# # # # # # # # # Plot Pertama: Membuat Line Chart Trend Pertumbuhan GMV
+
 # In[5]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
-# # # # # # # # # 
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+plt.plot(monthly_amount['order_month'], monthly_amount['gmv'])
+plt.show()
+
+# # # # # # # # # Cara Alternatif: Fungsi .plot() pada pandas Dataframe
+
 # In[6]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
 
-# # # # # # # # # 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.show()
+
+# # # # # # # # # Mengubah Figure Size
+
 # In[7]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+plt.figure(figsize=(15,5))
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.show()
+
+# # # # # # # # # Menambahkan Title and Axis Labels
+
 # In[8]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.title('Monthly GMV Year 2019')
+plt.xlabel('Order Month')
+plt.ylabel('Total GMV')
+plt.show()
+
+# # # # # # # # # Kustomisasi Title and Axis Labels
+
 # In[9]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount', fontsize=15)
+plt.show()
+
+# # # # # # # # # Kustomisasi Line dan Point
+
 # In[10]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount', fontsize=15)
+plt.show()
+
+# # # # # # # # # Kustomisasi Grid
+
 # In[11]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.show()
+
+# # # # # # # # # Kustomisasi Axis Ticks
+
 # In[12]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.show()
+
+# # # # # # # # # Menentukan Batas Minimum dan Maksimum Axis Ticks
+
 # In[13]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.show()
+
+# # # # # # # # # Menambahkan Informasi Pada Plot
+
 # In[14]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+fig = plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.text(0.45, 0.72, 'The GMV increased significantly on October 2019', transform=fig.transFigure, color='red')
+plt.show()
+
+# # # # # # # # # Menyimpan Hasil Plot Menjadi File Image
+
 # In[15]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+fig = plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.text(0.45,0.72, 'The GMV increased significantly on October 2019', transform=fig.transFigure, color='red')
+plt.savefig('monthly_gmv.png'
+)
+plt.show()
+
+# # # # # # # # # Pengaturan Parameter untuk Menyimpan Gambar
+
 # In[16]:
 
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
-# # # # # # # # # 
+fig = plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.text(0.45,0.72, 'The GMV increased significantly on October 2019', transform=fig.transFigure, color='red')
+plt.savefig('monthly_gmv.png', quality=95)
+plt.show()
+
+# # # # # # # # # Studi Kasus dari Senja: Daily number of customers on Desember
+
 # In[17]:
 
+# Import library yang dibutuhkan
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+# Baca dataset https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+# Buat kolom order_month
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+# Buat kolom gmv
+dataset['gmv'] = dataset['item_price'] * dataset['quantity']
+# Plot grafik sesuai dengan instruksi
+plt.figure(figsize=(10, 5))dataset[dataset['order_month']=='2019-12'].groupby(['order_date'])['customer_id'].nunique().plot(color='red', marker='.', linewidth=2)
 
-
-# # # # # # # # # 
-# In[18]:
-
-
-
-# # # # # # # # # 
-# In[19]:
-
-
-
-# # # # # # # # # 
-# In[20]:
-
-
-
-# # # # # # # # # 
-# In[21]:
-
-
-
-# # # # # # # # # 
-# In[22]:
-
-
-
-# # # # # # # # # 
-# In[23]:
-
-
-
-# # # # # # # # # 
-# In[24]:
-
-
-
-# # # # # # # # # 
-# In[25]:
-
-
-
-# # # # # # # # # 
-# In[26]:
-
-
-
-# # # # # # # # # 
-# In[27]:
-
-
-
-# # # # # # # # # 
-# In[28]:
-
-
-
-# # # # # # # # # 
-# In[29]:
-
-
-
-# # # # # # # # # 
-# In[30]:
-
-
-
-# # # # # # # # # 
-# In[31]:
-
-
-
-# # # # # # # # # 
-# In[32]:
-
-
-
-# # # # # # # # # 
-# In[33]:
-
-
-
-# # # # # # # # # 
-# In[34]:
-
-
-
-# # # # # # # # # 
-# In[35]:
-
-
-
-# # # # # # # # # 
-# In[36]:
-
-
-
+plt.title('Daily Number of Customers - December 2019', loc='left', pad=20, fontsize=20, color='orange')
+plt.xlabel('Order Date', fontsize=15, color='blue')
+plt.ylabel('Number of Customers', fontsize=15, color='blue')
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+plt.show()
