65667824f8d18e796edc80ae006f5300cae71803
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 17:17:06 +0700

    Learn

--- Courses/Python/2 Kelas Fundamental/Mengenal Model Regresi Linear Pada Python.py
+++ Courses/Python/2 Kelas Fundamental/Mengenal Model Regresi Linear Pada Python.py
@@ -1,185 +1,402 @@
-# # # # # # # # # 
+# # # # # # # # # Membaca Dataset
+
 # In[1]:
 
 
+# import pandas sebagai aliasnya pd
+import pandas as pd
+
+# Baca dataset kunjungan_dokter_gigi_kota_x_dqlab.tsv sesuai dengan url yang diberikan
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+# Cetak ukuran dataset sehingga diketahui jumlah baris dan kolomnya
+print("Ukuran df_kunjungan:", df_kunjungan.shape)
+# Cetak data frame df_kunjungan
+print(df_kunjungan)
+
+# Baca dataset tingkat_penjualan_kota_x_dqlab.tsv sesuai dengan url yang diberikan
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Cetak ukuran dataset sehingga diketahui jumlah baris dan kolomnya
+print("\n\nUkuran df_penjualan:", df_penjualan.shape)
+# Cetak data frame df_penjualan
+print(df_penjualan)
 
 
+# # # # # # # # # Penggabungan Kolom Tahun dan Bulan
 
-# # # # # # # # # 
 # In[2]:
 
+# Import pandas 
+import pandas as pd
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+
+# Inspeksi nama-nama kolom di kedua dataframe
+print("Nama kolom df_kunjungan:", df_kunjungan.columns)
+print("\nNama kolom df_penjualan", df_penjualan.columns)
 
 
 
 
-# # # # # # # # # 
+# # # # # # # # # Penggabungan Kedua Dataset
+
 # In[3]:
 
 
+# Import pandas 
+import pandas as pd
+pd.set_option("display.max_columns",5)
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
+print("Ukuran df:", df.shape)
+print(df)
 
 
 
-# # # # # # # # # 
+# # # # # # # # # Korelasi Penjualan Makanan dengan Kunjungan ke Dokter Gigi: Visualisasi
+
 # In[4]:
 
+# Import pandas 
+import pandas as pd
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
+
+# Import matplotlib.pyplot sebagai alias
+import matplotlib.pyplot as plt
+# Kolom x adalah kolom dengan awalan penjualan
+x_cols = df.columns[1:]
+# Kolom y merupakan kolom tingkat kunjungan ke dokter gigi
+y_col = "tingkat kunjungan ke dokter gigi"
+# Buat visualisasi 
+fig, axs = plt.subplots(len(x_cols), 1, figsize=(8,12))
+for ax, x_col in zip(axs, x_cols):
+    # Plotkan dengan scatter
+    df.plot.scatter(x=x_col, y=y_col, c="darkcyan", s=60, ax=ax)
+    ax.set_xlabel(x_col, fontsize=14)
+    ax.set_ylabel(y_col, fontsize=14)
+    ax.grid()
+plt.tight_layout()
+plt.show()
 
 
-# # # # # # # # # 
+# # # # # # # # # Korelasi Penjualan Makanan dengan Kunjungan ke Dokter Gigi: Koefisien Korelasi Pearson
+
 # In[5]:
 
+# Import pandas 
+import pandas as pd
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
 
-# # # # # # # # # 
+print("Koefisien korelasi Pearson:")
+print("---------------------------")
+print(df.corr()["tingkat kunjungan ke dokter gigi"])
+
+
+# # # # # # # # # The Delayed Effect
+
 # In[6]:
 
+# Import pandas 
+import pandas as pd
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
 
 
-# # # # # # # # # 
+import matplotlib.pyplot as plt
+# import stats dari scipy
+from scipy import stats
+
+# Spesifikasi keterlambatan hingga 8 bulan
+n_delays = 8
+# Spesifikasi jumlah kolom untuk subplots
+n_plt_cols = 2
+fig, axs = plt.subplots(round(n_delays/2), n_plt_cols, figsize=(12, 3.5*round(n_delays/2)), sharey=True)
+for i in range(n_delays):
+    # Ingat: varibel bebas dan variabel bergantung harus memiliki ukuran yang sama
+    # Ambilah variabel bebas: penjualan permen
+    x = df["penjualan permen"][:-(i+1)]
+    # Ambilah variabel bergantung: tingkat kunjungan ke dokter gigi dan implementasikan keterlambatan
+    y = df["tingkat kunjungan ke dokter gigi"][i+1:]
+    # Hitung koefisien korelasi Pearson
+    pearson_r, p_value = stats.pearsonr(x, y)
+    # Buat title setiap subplots dan juga tambatkan nilai  
+    # koefisien korelasi Pearson yang telah dihitung
+    title = "Efek keterlambatan %d bulan (r = %.2f)" % (i+1, pearson_r)
+    
+    # Buat scatter plot
+    ax = axs[i // n_plt_cols][i % n_plt_cols]
+    ax.scatter(x, y, c="darkcyan")
+    ax.set_title(title, fontsize=14, color="maroon")
+    if i % n_plt_cols == 0:
+        ax.set_ylabel("tingkat kunjungan ke dokter gigi", fontsize=12)
+    ax.set_xlabel("penjualan permen", fontsize=12)
+    ax.set_xlim([120000, 480000])
+    ax.grid()
+
+plt.tight_layout()
+plt.show()
+
+
+# # # # # # # # # Menentukan Persamaan Linier
+
 # In[7]:
 
+# Import pandas 
+import pandas as pd
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
+
+# Import numpy sebagai aliasnya np
+import numpy as np
+# Ambillah variabel bebas dan bergantung df untuk keterlambatan 4 bulan
+# dan ubahlah menjadi numpy 1d narray
+x = df["penjualan permen"][:-4].to_numpy()
+y = df["tingkat kunjungan ke dokter gigi"][4:].to_numpy()
+
+# Hitunglah nilai rata-rata variabel bebas dan bergantung
+x_mean = x.mean()
+y_mean = y.mean()
+
+# Hitung nilai pembilang dan penyebut untuk m
+m_pembilang = ((x - x_mean) * (y - y_mean)).sum()
+m_penyebut = ((x - x_mean)**2).sum()
+
+# Hitung nilai koefisien regresi linier
+m = m_pembilang / m_penyebut
+b = y_mean - m * x_mean
+print("Persamaan regresi linier: y = %.4e * x + %.4f" % (m, b))
 
 
-# # # # # # # # # 
+# # # # # # # # # Prediksi Tingkat Kunjungan
+
 # In[8]:
 
+# Import pandas dan numpy
+import pandas as pd
+import numpy as np
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
+# Ambillah variabel bebas dan bergantung df untuk keterlambatan 4 bulan
+# dan ubahlah menjadi numpy 1d narray
+x = df["penjualan permen"][:-4].to_numpy()
+y = df["tingkat kunjungan ke dokter gigi"][4:].to_numpy()
+# Hitunglah nilai rata-rata variabel bebas dan bergantung
+x_mean = x.mean()
+y_mean = y.mean()
+# Hitung nilai pembilang dan penyebut untuk m
+m_pembilang = ((x - x_mean) * (y - y_mean)).sum()
+m_penyebut = ((x - x_mean)**2).sum()
+# Hitung nilai koefisien regresi linier
+m = m_pembilang / m_penyebut
+b = y_mean - m * x_mean
+
+# Ambillah butir data variabel bebas yang belum digunakan
+x_new = df["penjualan permen"][-4:].to_numpy()
+#  Gunakan x_new ke dalam persamaan regresi
+y_pred = m * x_new + b
+print("Prediksi tingkat kunjungan ke dokter gigi 1998-01 s/d 1998-04:")
+for i, kunjungan in enumerate(y_pred):
+    print("1998-0%d: %4d kunjungan." % (i+1, round(kunjungan)))
 
 
-# # # # # # # # # 
+# # # # # # # # # Regresi Linier dengan Scikit-Learn
+
 # In[9]:
 
+# Import pandas 
+import pandas as pd
+import numpy as np
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
+
+# Importlah LinearRegression dari sklearn.linear_model
+from sklearn.linear_model import LinearRegression
+
+# Ambillah variabel bebas dan bergantung untuk keterlambatan 4 bulan
+# dan ubahlah menjadi numpy 2d narray melalui .reshape((-1,1))
+x = df["penjualan permen"][:-4].to_numpy().reshape((-1,1))
+y = df["tingkat kunjungan ke dokter gigi"][4:].to_numpy().reshape((-1,1))
+
+# Instansiasi LinearRegression ke dalam lr
+lr = LinearRegression()
+# Terapkan method fit pada variabel bebas dan bergantung
+lr.fit(x,y)
+
+# Ambillah butir data variabel bebas yang belum digunakan
+# dan ubahlah menjadi numpy 2d narray melalui .reshape((-1,1))
+x_new = df["penjualan permen"][-4:].to_numpy().reshape((-1,1))
+# Prediksilah x_new dengan method predict
+y_pred = lr.predict(x_new)
+
+print("Persamaan regresi linier: y = %.4e * x + %.4f\n" % (lr.coef_, lr.intercept_))
+print("Prediksi tingkat kunjungan ke dokter gigi 1998-01 s/d 1998-04:")
+for i, kunjungan in enumerate(y_pred):
+    print("1998-0%d: %4d kunjungan." % (i+1, round(kunjungan[0])))
 
 
-# # # # # # # # # 
+# # # # # # # # # Mini Project
+
 # In[10]:
 
-
-
-# # # # # # # # # 
-# In[11]:
-
-
-
-# # # # # # # # # 
-# In[12]:
-
-
-
-# # # # # # # # # 
-# In[13]:
-
-
-
-# # # # # # # # # 
-# In[14]:
-
-
-
-# # # # # # # # # 
-# In[15]:
-
-
-
-# # # # # # # # # 
-# In[16]:
-
-
-
-# # # # # # # # # 
-# In[17]:
-
-
-
-# # # # # # # # # 
-# In[18]:
-
-
-
-# # # # # # # # # 
-# In[19]:
-
-
-
-# # # # # # # # # 
-# In[20]:
-
-
-
-# # # # # # # # # 
-# In[21]:
-
-
-
-# # # # # # # # # 
-# In[22]:
-
-
-
-# # # # # # # # # 
-# In[23]:
-
-
-
-# # # # # # # # # 
-# In[24]:
-
-
-
-# # # # # # # # # 
-# In[25]:
-
-
-
-# # # # # # # # # 
-# In[26]:
-
-
-
-# # # # # # # # # 
-# In[27]:
-
-
-
-# # # # # # # # # 
-# In[28]:
-
-
-
-# # # # # # # # # 
-# In[29]:
-
-
-
-# # # # # # # # # 
-# In[30]:
-
-
-
-# # # # # # # # # 
-# In[31]:
-
-
-
-# # # # # # # # # 
-# In[32]:
-
-
-
-# # # # # # # # # 
-# In[33]:
-
-
-
-# # # # # # # # # 
-# In[34]:
-
-
-
-# # # # # # # # # 
-# In[35]:
-
-
-
-# # # # # # # # # 
-# In[36]:
-
-
+# Import pandas
+import pandas as pd
+# Baca kedua dataset
+df_kunjungan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", sep="\t")
+df_penjualan = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", sep="\t")
+# Gabungkan kolom Tahun dan Bulan menjadi kolom Periode dengan isi tiap barisnya memiliki format YYYY-MM 
+str_bulan = lambda x: "0"+str(x) if x<10 else str(x)
+df_kunjungan["Periode"] = df_kunjungan["Tahun"].map(str) + "-" + df_kunjungan["Bulan"].map(str_bulan)
+df_penjualan["Periode"] = df_penjualan["Tahun"].map(str) + "-" + df_penjualan["Bulan"].map(str_bulan)
+# Drop kolom Tahun, Bulan dari df_kunjungan
+df_kunjungan.drop(columns=["Tahun", "Bulan"], inplace=True)
+# Drop kolom Tahun, Bulan dan No dari df_penjualan
+df_penjualan.drop(columns=["Tahun", "Bulan", "No"], inplace=True)
+# Set index kolom Periode
+df_kunjungan.set_index("Periode", inplace=True)
+df_penjualan.set_index("Periode", inplace=True)
+# Gabungkan kedua dataframe dengan Periode yang telah menjadi key column nya
+df = df_kunjungan.join(df_penjualan)
+
+# Importlah numpy sebagai np dan stats dari scipy
+import numpy as np
+from scipy import stats
+
+# Ambillah variabel bebas dan bergantung untuk keterlambatan 4 bulan
+# dan ubahlah menjadi numpy 1d narray
+x = df["penjualan permen"][:-4].to_numpy()
+y = df["tingkat kunjungan ke dokter gigi"][4:].to_numpy()
+
+# Penggunaan numpy.polyfit
+print("Penggunaan numpy.polyfit:")
+np_out = np.polyfit(x, y, 1)
+print("  Persamaan regresi linier: y = %.4e * x + %.4f\n" % (np_out[0], np_out[1]))
+
+# Penggunaan stats.linregress
+print("Penggunaan stats.linregress dari scipy:")
+sclr_out = stats.linregress(x, y)
+print("  Persamaan regresi linier: y = %.4e * x + %.4f\n" % (sclr_out.slope, sclr_out.intercept))
+
+# Gunakan salah satu nilai saja dari np_out atau sclr_out
+# untuk koefisien regresi liniernya (gunakan sclr_out)
+
+# Ambillah butir data variabel bebas yang belum digunakan
+x_new = df["penjualan permen"][-4:].to_numpy()
+#  Gunakan x_new ke dalam persamaan regresi linier
+y_pred = sclr_out.slope * x_new + sclr_out.intercept
+print("Prediksi tingkat kunjungan ke dokter gigi 1998-01 s/d 1998-04:")
+for i, kunjungan in enumerate(y_pred):
+    print("1998-0%d: %4d kunjungan." % (i+1, round(kunjungan)))
 
