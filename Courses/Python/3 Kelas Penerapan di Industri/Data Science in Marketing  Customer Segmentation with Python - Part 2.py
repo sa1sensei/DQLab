ae24af16977bf1e387611543c22c5f28ffd04776
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 18:16:15 +0700

    Learn

--- Courses/Python/3 Kelas Penerapan di Industri/Data Science in Marketing  Customer Segmentation with Python - Part 2.py
+++ Courses/Python/3 Kelas Penerapan di Industri/Data Science in Marketing  Customer Segmentation with Python - Part 2.py
@@ -1,185 +1,439 @@
-# # # # # # # # # 
+# # # # # # # # # Mencari Jumlah Cluster yang Optimal
+
 # In[1]:
 
 
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt  
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+
+# Melakukan Iterasi untuk Mendapatkan nilai Cost  
+cost = {}
+for k in range(2,10):
+  kproto = KPrototypes(n_clusters = k,random_state=75)
+  kproto.fit_predict(df_model, categorical=[0,1,2])
+  cost[k]= kproto.cost_
+  
+# Memvisualisasikan Elbow Plot
+sns.pointplot(x=list(cost.keys()), y=list(cost.values()))
+plt.show()
 
 
 
-# # # # # # # # # 
+# # # # # # # # # Membuat Model
+
 # In[2]:
 
 
+import pandas as pd 
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+
+import pickle
+from kmodes.kmodes import KModes
+from kmodes.kprototypes import KPrototypes
+
+kproto = KPrototypes(n_clusters=5, random_state = 75)
+kproto = kproto.fit(df_model, categorical=[0,1,2])
+
+#Save Model
+pickle.dump(kproto, open('cluster.pkl', 'wb'))
 
 
 
-# # # # # # # # # 
+# # # # # # # # # Menggunakan Model
+
 # In[3]:
 
+import pandas as pd
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t") 
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+kproto = KPrototypes(n_clusters=5, random_state = 75)  
+kproto = kproto.fit(df_model, categorical=[0,1,2])  
+
+# Menentukan segmen tiap pelanggan    
+clusters = kproto.predict(df_model, categorical=[0,1,2])
+print('segmen pelanggan: {}\n'.format(clusters))
+
+# Menggabungkan data awal dan segmen pelanggan    
+df_final = df.copy()
+df_final['cluster'] = clusters
+print(df_final.head())
 
 
 
+# # # # # # # # # Menampilkan Cluster Tiap Pelanggan
 
-# # # # # # # # # 
 # In[4]:
 
+import pandas as pd
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/customer_segments.txt', sep='\t') 
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+kproto = KPrototypes(n_clusters=5, random_state = 75)  
+kproto = kproto.fit(df_model, categorical=[0,1,2])  
+
+clusters = kproto.predict(df_model, categorical=[0,1,2]) 
+df_final = df.copy()    
+df_final['cluster'] = clusters 
+
+# Menampilkan data pelanggan berdasarkan cluster nya  
+for i in range (0,5):
+    print('\nPelanggan cluster: {}\n'.format(i))
+    print(df_final[df_final['cluster']== i])
 
 
-# # # # # # # # # 
+# # # # # # # # # Visualisasi Hasil Clustering - Box Plot
+
 # In[5]:
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t") 
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+kproto = KPrototypes(n_clusters=5, random_state = 75)  
+kproto = kproto.fit(df_model, categorical=[0,1,2])  
 
-# # # # # # # # # 
+clusters =  kproto.predict(df_model, categorical=[0,1,2]) 
+df_final = df.copy()    
+df_final['cluster'] = clusters 
+
+# Data Numerical
+kolom_numerik = ['Umur','NilaiBelanjaSetahun']  
+  
+for i in kolom_numerik:
+    plt.figure(figsize=(6,4))
+    ax = sns.boxplot(x = 'cluster',y = i, data = df_final)
+    plt.title('\nBox Plot {}\n'.format(i), fontsize=12)
+    plt.show()
+
+
+
+# # # # # # # # # Visualisasi Hasil Clustering - Count Plot
+
 # In[6]:
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t") 
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+kproto = KPrototypes(n_clusters=5, random_state = 75)  
+kproto = kproto.fit(df_model, categorical=[0,1,2])  
 
+clusters =  kproto.predict(df_model, categorical=[0,1,2]) 
+df_final = df.copy()    
+df_final['cluster'] = clusters 
 
-# # # # # # # # # 
+# Data Kategorikal  
+kolom_categorical = ['Jenis Kelamin','Profesi','Tipe Residen']
+
+for i in kolom_categorical:
+    plt.figure(figsize=(6,4))
+    ax = sns.countplot(data = df_final, x = 'cluster', hue = i )
+    plt.title('\nCount Plot {}\n'.format(i), fontsize=12)
+    ax.legend(loc="upper center")
+for p in ax.patches:
+    ax.annotate(format(p.get_height(), '.0f'),
+    (p.get_x() + p.get_width() / 2., p.get_height()),
+    ha = 'center',
+    va = 'center',
+    xytext = (0, 10),
+    textcoords = 'offset points')
+
+sns.despine(right=True,top = True, left = True)
+ax.axes.yaxis.set_visible(False)
+plt.show()
+
+# # # # # # # # # Menamakan Cluster
+
 # In[7]:
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t") 
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+kproto = KPrototypes(n_clusters=5, random_state = 75)  
+kproto = kproto.fit(df_model, categorical=[0,1,2])  
 
+clusters =  kproto.predict(df_model, categorical=[0,1,2]) 
+df_final = df.copy()    
+df_final['cluster'] = clusters 
 
-# # # # # # # # # 
+# Mapping nama kolom
+df_final['segmen'] = df_final['cluster'].map({
+0: 'Diamond Young Member',
+1: 'Diamond Senior Member',
+2: 'Silver Member',
+3: 'Gold Young Member',
+4: 'Gold Senior Member'
+})
+
+print(df_final.info())
+print(df_final.head())
+
+# # # # # # # # # Mempersiapkan Data Baru
+
 # In[8]:
 
+import pandas as pd
+# Data Baru
+data = [{
+    'Customer_ID': 'CUST-100' ,
+    'Nama Pelanggan': 'Joko' ,
+    'Jenis Kelamin': 'Pria',
+    'Umur': 45,
+    'Profesi': 'Wiraswasta',
+    'Tipe Residen': 'Cluster' ,
+    'NilaiBelanjaSetahun': 8230000
+}]
+
+# Membuat Data Frame
+new_df = pd.DataFrame(data)
+
+# Melihat Data
+print(new_df)
 
 
-# # # # # # # # # 
+
+# # # # # # # # # Membuat Fungsi Data Pemrosesan
+
 # In[9]:
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t") 
+df_model = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/df-customer-segmentation.csv')
+kproto = KPrototypes(n_clusters=5, random_state = 75)  
+kproto = kproto.fit(df_model, categorical=[0,1,2])  
+
+clusters =  kproto.predict(df_model, categorical=[0,1,2]) 
+df_final = df.copy()    
+df_final['cluster'] = clusters 
+df_final['segmen'] = df_final['cluster'].map({  
+    0: 'Diamond Young Member',  
+    1: 'Diamond Senior Member',  
+    2: 'Silver Member',  
+    3: 'Gold Young Member',  
+    4: 'Gold Senior Member'  
+})  
+
+data = [{  
+    'Customer_ID': 'CUST-100' ,  
+    'Nama Pelanggan': 'Joko' ,  
+    'Jenis Kelamin': 'Pria',  
+    'Umur': 45,  
+    'Profesi': 'Wiraswasta',  
+    'Tipe Residen': 'Cluster' ,  
+    'NilaiBelanjaSetahun': 8230000  
+      
+}]
+new_df = pd.DataFrame(data) 
+
+def data_preprocess(data):
+    # Konversi Kategorikal data
+    kolom_kategorikal = ['Jenis Kelamin','Profesi','Tipe Residen']
+
+    df_encode = data[kolom_kategorikal].copy()
+
+  
+ 	## Jenis Kelamin
+    df_encode['Jenis Kelamin'] = df_encode['Jenis Kelamin'].map({
+    'Pria': 0,
+    'Wanita' : 1
+    })
+
+    ## Profesi
+    df_encode['Profesi'] = df_encode['Profesi'].map({
+    'Ibu Rumah Tangga': 0,
+    'Mahasiswa' : 1,
+    'Pelajar': 2,
+    'Professional': 3,
+    'Wiraswasta': 4
+    })
+
+    ## Tipe Residen
+    df_encode['Tipe Residen'] = df_encode['Tipe Residen'].map({
+    'Cluster': 0,
+    'Sector' : 1
+    })
+
+    # Standardisasi Numerical Data
+    kolom_numerik = ['Umur','NilaiBelanjaSetahun']
+    df_std = data[kolom_numerik].copy()
+
+    ## Standardisasi Kolom Umur
+    df_std['Umur'] = (df_std['Umur'] - 37.5)/14.7
+
+    ## Standardisasi Kolom Nilai Belanja Setahun
+    df_std['NilaiBelanjaSetahun'] = (df_std['NilaiBelanjaSetahun'] - 7069874.8)/2590619.0
+
+    # Menggabungkan Kategorikal dan numerikal data
+    df_model = df_encode.merge(df_std, left_index = True,
+    right_index=True, how = 'left')
+
+    return df_model
+  
+# Menjalankan fungsi
+new_df_model = data_preprocess(new_df)
+
+print(new_df_model)
 
 
-# # # # # # # # # 
+
+
+# # # # # # # # # Memanggil Model dan Melakukan Prediksi
+
 # In[10]:
 
+import pickle
+import pandas as pd
+data = [{  
+    'Customer_ID': 'CUST-100' ,  
+    'Nama Pelanggan': 'Joko' ,  
+    'Jenis Kelamin': 'Pria',  
+    'Umur': 45,  
+    'Profesi': 'Wiraswasta',  
+    'Tipe Residen': 'Cluster' ,  
+    'NilaiBelanjaSetahun': 8230000  
+      
+}]
+new_df = pd.DataFrame(data) 
+
+def data_preprocess(data):
+    kolom_kategorikal = ['Jenis Kelamin','Profesi','Tipe Residen']
+    df_encode = data[kolom_kategorikal].copy()
+    df_encode['Jenis Kelamin'] = df_encode['Jenis Kelamin'].map({  
+        'Pria': 0,  
+        'Wanita' : 1  
+    })
+    df_encode['Profesi'] = df_encode['Profesi'].map({  
+        'Ibu Rumah Tangga': 0,  
+        'Mahasiswa' : 1,  
+        'Pelajar': 2,  
+        'Professional': 3,  
+        'Wiraswasta': 4  
+    }) 
+    df_encode['Tipe Residen'] = df_encode['Tipe Residen'].map({  
+        'Cluster': 0,  
+        'Sector' : 1  
+    }) 
+    kolom_numerik = ['Umur','NilaiBelanjaSetahun']  
+    df_std = data[kolom_numerik].copy()
+    df_std['Umur'] = (df_std['Umur'] - 37.5)/14.7 
+    df_std['NilaiBelanjaSetahun'] = (df_std['NilaiBelanjaSetahun'] - 7069874.8)/2590619.0
+    df_model = df_encode.merge(df_std, left_index = True,  
+                           right_index=True, how = 'left')  
+    return df_model  
+  
+new_df_model = data_preprocess(new_df)
+
+def modelling (data):
+
+    # Memanggil Model
+    kpoto = pickle.load(open('cluster.pkl', 'rb'))
+
+    # Melakukan Prediksi
+    clusters = kpoto.predict(data,categorical=[0,1,2])
+
+    return clusters
+
+# Menjalankan Fungsi
+clusters = modelling(new_df_model)
+
+print(clusters) 
 
 
-# # # # # # # # # 
+
+
+# # # # # # # # # Menamakan Segmen
+
 # In[11]:
 
 
-
-# # # # # # # # # 
-# In[12]:
-
-
-
-# # # # # # # # # 
-# In[13]:
-
-
-
-# # # # # # # # # 
-# In[14]:
-
-
-
-# # # # # # # # # 
-# In[15]:
-
-
-
-# # # # # # # # # 
-# In[16]:
-
-
-
-# # # # # # # # # 
-# In[17]:
-
-
-
-# # # # # # # # # 
-# In[18]:
-
-
-
-# # # # # # # # # 
-# In[19]:
-
-
-
-# # # # # # # # # 
-# In[20]:
-
-
-
-# # # # # # # # # 
-# In[21]:
-
-
-
-# # # # # # # # # 
-# In[22]:
-
-
-
-# # # # # # # # # 
-# In[23]:
-
-
-
-# # # # # # # # # 
-# In[24]:
-
-
-
-# # # # # # # # # 
-# In[25]:
-
-
-
-# # # # # # # # # 
-# In[26]:
-
-
-
-# # # # # # # # # 
-# In[27]:
-
-
-
-# # # # # # # # # 
-# In[28]:
-
-
-
-# # # # # # # # # 
-# In[29]:
-
-
-
-# # # # # # # # # 
-# In[30]:
-
-
-
-# # # # # # # # # 
-# In[31]:
-
-
-
-# # # # # # # # # 
-# In[32]:
-
-
-
-# # # # # # # # # 
-# In[33]:
-
-
-
-# # # # # # # # # 
-# In[34]:
-
-
-
-# # # # # # # # # 
-# In[35]:
-
-
-
-# # # # # # # # # 
-# In[36]:
-
-
-
+import pickle
+import pandas as pd
+data = [{  
+    'Customer_ID': 'CUST-100' ,  
+    'Nama Pelanggan': 'Joko' ,  
+    'Jenis Kelamin': 'Pria',  
+    'Umur': 45,  
+    'Profesi': 'Wiraswasta',  
+    'Tipe Residen': 'Cluster' ,  
+    'NilaiBelanjaSetahun': 8230000  
+      
+}]
+new_df = pd.DataFrame(data) 
+
+def data_preprocess(data):
+    kolom_kategorikal = ['Jenis Kelamin','Profesi','Tipe Residen']
+    df_encode = data[kolom_kategorikal].copy()
+    df_encode['Jenis Kelamin'] = df_encode['Jenis Kelamin'].map({  
+        'Pria': 0,  
+        'Wanita' : 1  
+    })
+    df_encode['Profesi'] = df_encode['Profesi'].map({  
+        'Ibu Rumah Tangga': 0,  
+        'Mahasiswa' : 1,  
+        'Pelajar': 2,  
+        'Professional': 3,  
+        'Wiraswasta': 4  
+    }) 
+    df_encode['Tipe Residen'] = df_encode['Tipe Residen'].map({  
+        'Cluster': 0,  
+        'Sector' : 1  
+    }) 
+    kolom_numerik = ['Umur','NilaiBelanjaSetahun']  
+    df_std = data[kolom_numerik].copy()
+    df_std['Umur'] = (df_std['Umur'] - 37.5)/14.7 
+    df_std['NilaiBelanjaSetahun'] = (df_std['NilaiBelanjaSetahun'] - 7069874.8)/2590619.0
+    df_model = df_encode.merge(df_std, left_index = True,  
+                           right_index=True, how = 'left')  
+    return df_model  
+  
+new_df_model = data_preprocess(new_df)  
+
+def modelling (data): 
+    kpoto = pickle.load(open('cluster.pkl', 'rb'))
+    clusters = kpoto.predict(data,categorical=[0,1,2])  
+    return clusters  
+    
+clusters = modelling(new_df_model)
+
+def menamakan_segmen (data_asli, clusters):  
+      
+    # Menggabungkan cluster dan data asli  
+  	final_df = data_asli.copy()
+    final_df['cluster'] = clusters
+      
+    # Menamakan segmen  
+    final_df['segmen'] = final_df['cluster'].map({  
+        0: 'Diamond Young Member',  
+        1: 'Diamond Senior Member',  
+        2: 'Silver Students',  
+        3: 'Gold Young Member',  
+        4: 'Gold Senior Member'  
+    })  
+      
+    return final_df
+
+# Menjalankan Fungsi
+new_final_df = menamakan_segmen(new_df,clusters)
+
+print(new_final_df)
