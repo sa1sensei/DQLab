aa42fb990222969fd4e07511d9263ac02858e4b5
sa1-sensei <sigitarisetiawan@gmail.com>
2023-01-16 17:06:51 +0700

    Learn

--- Courses/Python/2 Kelas Fundamental/Ukuran Data Variasi.py
+++ Courses/Python/2 Kelas Fundamental/Ukuran Data Variasi.py
@@ -1,7 +1,17 @@
-# # # # # # # # # 
+# # # # # # # # # Membaca Dataset
+
 # In[1]:
 
 
+# Import numpy sebagai aliasnya np
+import numpy as np
+
+# Baca dataset 
+tinggi_badan = np.loadtxt("https://storage.googleapis.com/dqlab-dataset/survei_tinggi_badan.txt", skiprows=1)
+
+# Cetak ukuran dataset dan isinya
+print("Ukuran data tinggi_badan:", tinggi_badan.shape)
+print("Data tinggi_badan (cm):\n", tinggi_badan)
 
 
 
